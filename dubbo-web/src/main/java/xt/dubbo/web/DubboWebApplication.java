c255c140079843c8af965d7daafd6cb2f70fc1c5
v_xiongtao <bigapple0829@163.com>
2017-09-06 17:31:28 +0800

    初始提交

---
+++ dubbo-web/src/main/java/xt/dubbo/web/DubboWebApplication.java
@@ -0,0 +1,12 @@
+package xt.dubbo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboWebApplication.class, args);
+	}
+}
